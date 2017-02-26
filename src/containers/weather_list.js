03c84159702761c70ebc9a3e2abd56048f04ac56
Joe Stowers <greenchiletexan@gmail.com>
2017-02-25 21:46:50 -0700

    Create renderForecast() method to render each row of data in the weather table.

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -6,7 +6,51 @@ import { connect } from 'react-redux';
 
 class WeatherList extends Component {
 
+	constructor(props) {
+		super(props);
+
+
+	}
+
+	renderForecast() {
+
+		// this.props.weather => [{ }, { }]
+		console.log('this.props.weather =', this.props.weather);
+
+		return this.props.weather.map((city, index) => {
+			return (
+				<tr key= {index}>
+					<td>
+						{city.name}
+					</td>
+					<td>
+						{city.main.temp}
+					</td>
+					<td>
+						{city.main.pressure}
+					</td>
+					<td>
+						{city.main.humidity}
+					</td>
+				</tr>
+			);
+		});
+	}
+
 	render() {
+
+		// props => returns an object with weather as a property
+		// the value of weather is an array, with each element 
+		// representing a city searched by the user
+		// the most recent city searched => this.props.weather[0]
+
+		// accessing a value for each city's weather property:
+		// => this.props.weather[0]{<value>}
+		// console.log('WeatherList props.weather', this.props.weather); 
+
+		// <tr> => defines a row
+		// <td> => defines a cell
+
 		return (
 			<table className = 'table table-hover'>
 				<thead>
@@ -18,8 +62,7 @@ class WeatherList extends Component {
 					</tr>
 				</thead>
 				<tbody>
-
-
+					{this.renderForecast()}
 				</tbody>
 			</table>
 		);
